0d145f0e47d6f58efd5e38dc4f5a2f7608ea0160
David Trott <github@davidtrott.com>
2021-01-13 22:42:31 -0800

    Added second.

--- vars/exampleFun.groovy
+++ vars/exampleFun.groovy
@@ -1,6 +1,6 @@
-def call(params) {
+def call(params, String second) {
 
     String name = params.name
     String value = params.value
-    echo "Name: ${name},  Value: ${value}"
+    echo "Name: ${name},  Value: ${value}, Second: ${second}"
 }
